12ed5a89e31df4897177eaab9883f553f52d2ca5
Minjunebae <103413040+bmj44571627@users.noreply.github.com>
2022-07-01 09:47:45 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,71 @@
+ import './App.css';
+import WeatherBox from './components/WeatherBox';
+import WeatherButton from './components/WeatherButton';
+import {Container} from 'react-bootstrap';
+import React, {useState}
+
+function App () {
+
+const getWeatherByCurrentLocation = async(lat, lon) =>{
+
+  try {
+    let url = 
+    `https://api.openweathermap.org/data/2.5/weather?lat=57&lon=-2.15&appid=547efbb8e2e0f9fb24c4ae6ad98c03bb&units=metric`;
+    
+
+    const res = await fetch(url);
+    const data = await res.json();
+
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+
+  const getCurrentLocation = () => {
+
+    navigator.geolocation.getCurrentPosition((position) => {
+
+      const { latitude, longitude } = position.coords;
+
+      getWeatherByCurrentLocation(latitude, longitude);
+
+      console.log('현재위치?', latitude, longitude);
+
+    });
+
+  };
+    
+  const getWeatherByCity = () => {
+    try {
+
+      let url = 'https://api.openweathermap.org/data/2.5/weather?q=${city}appid={547efbb8e2e0f9fb24c4ae6ad98c03bb}';
+
+    const res = await fetch(url);
+    const data = await res.json();
+    setWeather(data);
+    setLoading(false);
+    
+    }catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(()) => {
+    if(city == null){
+      get
+    }
+  });
+  return (  
+    <>  
+  <Container className="vh-100">
+    <div></div>
+    <div className="main-container">
+        <WeatherBox />
+        <WeatherButton />
+    </div>
+  </Container>
+  </>
+  );
+}
+export default App;  
